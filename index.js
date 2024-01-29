149c5be716f1a3bf498365e6fff19d0fee088be7
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-29 14:08:15 +0700

    menambah dan menghubungkan page biodata dan kemahasiswaan

--- index.js
+++ index.js
@@ -10,15 +10,23 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('views'));
 
+const isiBiodata = {
+    nim:'00000000',
+    nama:'Rekayasa Marganih',
+    fakultas:'Ilmu Komputer',
+    jurusan:'Teknik informatika',
+    prodi:'Teknik informatika',
+    seleksi:'mandiri'
+}
+
 app.get('/',(req,res)=>{
-    const isiBiodata = {
-        nim:'00000000',
-        nama:'Rekayasa Marganih',
-        fakultas:'Ilmu Komputer',
-        jurusan:'Teknik informatika',
-        prodi:'Teknik informatika',
-        seleksi:'mandiri'
-    }
     res.render('akademik', isiBiodata);
     
+})
+app.get('/biodata',(req,res)=>{
+    res.render('biodata',isiBiodata)
+})
+
+app.get('/kemahasiswaan',(req,res)=>{
+    res.render('kemahasiswaan',isiBiodata);
 })
